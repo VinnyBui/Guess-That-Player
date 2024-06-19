8ebc9f8f19ceb964b28e7f8662f797064939754c
Vinh B <vinh.bui0101@gmail.com>
2024-06-19 13:45:58 -0700

    Set so that result list only shows when Search bar is selected

--- Guess-That-Player/components/searchPlayer.jsx
+++ Guess-That-Player/components/searchPlayer.jsx
@@ -1,12 +1,27 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import fullName from './fullName.jsx';
-import { FaSearch } from "react-icons/fa"
+import { FaSearch } from "react-icons/fa";
 
-function SearchPlayer({playersData}) {
+function SearchPlayer({ playersData }) {
     const [input, setInput] = useState("");
-    let fullNameList = fullName(playersData);
+    const [showResults, setShowResults] = useState(false); // State to manage the visibility of results
+    const fullNameList = fullName(playersData);
 
-    const filteredNamesList = fullNameList.filter(name => name.toLowerCase().includes(input.toLowerCase()))
+    // Filter the list of names based on the input
+    const filteredNames = fullNameList.filter(name =>
+        name.toLowerCase().includes(input.toLowerCase())
+    );
+
+    // Event handlers to show and hide the results list
+    const handleFocus = () => {
+        setShowResults(true);
+    };
+
+    const handleBlur = () => {
+        // Use a timeout to ensure click event on the result list gets registered before hiding it
+        setTimeout(() => setShowResults(false), 100);
+    };
+
     return (
         <>
             <div className="Search-bar-container">
@@ -15,17 +30,21 @@ function SearchPlayer({playersData}) {
                     <input 
                         type="text" 
                         placeholder="Name"
-                        value = {input}
-                        onChange= {(e) => setInput(e.target.value)}
+                        value={input}
+                        onFocus={handleFocus}
+                        onBlur={handleBlur}
+                        onChange={(e) => setInput(e.target.value)}
                     />
                 </div>
-                <div className="Results-list">
-                    {filteredNamesList.map((name, index) => (
-                        <div className="Search-result" key={index}>
-                            <p>{name}</p>
-                        </div>
-                    ))}
-                </div>
+                {showResults && (
+                    <div className="Results-list">
+                        {filteredNames.map((name, index) => (
+                            <div className="Search-result" key={index}>
+                                <p>{name}</p>
+                            </div>
+                        ))}
+                    </div>
+                )}
             </div>
         </>
     );
